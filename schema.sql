6e1e423cb301400765e9ac90b05dfd874ae5b125
you know who <chorvickprocode@gmail.com>
2021-03-16 15:57:55 -0400

    server starts and connects - many functions to create still

--- schema.sql
+++ schema.sql
@@ -1,2 +1,6 @@
 -- our schema will go here to create a company database that will have three tables ---
 -- a table for departmet, role and, employee --
+DROP DATABASE IF EXISTS companyDB;
+CREATE DATABASE companyDB;
+---drop and/or delete if it exists if not create a new database called company
+USE companyDB;
